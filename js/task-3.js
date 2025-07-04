d7b7f35e034b786dba7e38c3e0071154349e8998
Igor602437 <a505741747@gmail.com>
2025-07-04 22:29:54 +0300

    remove-clg-hw-7

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,9 @@
 const inputName = document.querySelector('#name-input');
-console.log(inputName.textContent);
 const outputName = document.querySelector('#name-output');
-console.log(outputName);
 
 inputName.addEventListener('input', event => {
   const inputText = event.currentTarget.value;
-  console.log(inputText);
+
   if (inputText.trim() === '') {
     outputName.textContent = 'Anonymous';
   } else {
